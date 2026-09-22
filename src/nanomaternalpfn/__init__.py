8ce0e25a49a0574cbc1d5555ecfba60b981d8fea
ζ(s) = ∑ₙ₌₁^∞ 1/nˢ. <PeluxzGlitterati@gmail.com>
2026-09-22 13:02:34 +0100

    Export nanoMaternalPFN model

--- src/nanomaternalpfn/__init__.py
+++ src/nanomaternalpfn/__init__.py
@@ -1,5 +1,12 @@
 """nanoMaternalPFN research package."""
 
+from .model import NanoMaternalPFN, count_parameters
 from .synthetic import FEATURE_NAMES, SyntheticMaternalTask, generate_task
 
-__all__ = ["FEATURE_NAMES", "SyntheticMaternalTask", "generate_task"]
+__all__ = [
+    "FEATURE_NAMES",
+    "NanoMaternalPFN",
+    "SyntheticMaternalTask",
+    "count_parameters",
+    "generate_task",
+]
